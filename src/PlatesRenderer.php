209d58a41b80e5fcec97c606cc3e8afd37cd4c19
ncou <nicolas_courade@yahoo.fr>
2018-09-05 18:43:24 +0200

    first commit

---
+++ src/PlatesRenderer.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Views;
+
+use Chiron\Views\AttributesTrait;
+use Chiron\Views\TemplateRendererInterface;
+use Chiron\Views\TemplatePath;
+
+use League\Plates\Engine;
+
+class PlatesRenderer implements TemplateRendererInterface
+{
+    use AttributesTrait;
+
+    /**
+     * @var Engine
+     */
+    private $engine;
+
+    public function __construct(Engine $engine = null)
+    {
+        $this->engine = $engine ?: $this->createPlatesEngine();
+    }
+    /**
+     * Create a default Plates engine
+     */
+    private function createPlatesEngine() : Engine
+    {
+        return new Engine();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function render(string $name, array $params = []) : string
+    {
+        //$params = $this->normalizeParams($params);
+        $params = array_merge($this->attributes, $params);
+        return $this->engine->render($name, $params);
+    }
+    /**
+     * Add a path for template
+     *
+     * Multiple calls to this method without a namespace will trigger an
+     * E_USER_WARNING and act as a no-op. Plates does not handle non-namespaced
+     * folders, only the default directory; overwriting the default directory
+     * is likely unintended.
+     */
+    public function addPath(string $path, string $namespace = null) : void
+    {
+        if (! $namespace && ! $this->engine->getDirectory()) {
+            $this->engine->setDirectory($path);
+            return;
+        }
+        if (! $namespace) {
+            trigger_error('Cannot add duplicate un-namespaced path in Plates template adapter', E_USER_WARNING);
+            return;
+        }
+        $this->engine->addFolder($namespace, $path);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getPaths() : array
+    {
+        $paths = $this->engine->getDirectory()
+            ? [ $this->getDefaultPath() ]
+            : [];
+        foreach ($this->getPlatesFolders() as $folder) {
+            $paths[] = new TemplatePath($folder->getPath(), $folder->getName());
+        }
+        return $paths;
+    }
+
+    /**
+     * Checks if the view exists
+     *
+     * @param   string  $name  Full template path or part of a template path
+     *
+     * @return  boolean  True if the path exists
+     */
+    public function exists(string $name): bool
+    {
+        return $this->engine->exists($name);
+    }
+    /**
+     * Create and return a TemplatePath representing the default Plates directory.
+     */
+    private function getDefaultPath() : TemplatePath
+    {
+        return new TemplatePath($this->engine->getDirectory());
+    }
+    /**
+     * Return the internal array of plates folders.
+     *
+     * @return \League\Plates\Template\Folder[]
+     */
+    private function getPlatesFolders() : array
+    {
+        $folders = $this->engine->getFolders();
+        $r = new \ReflectionProperty($folders, 'folders');
+        $r->setAccessible(true);
+        return $r->getValue($folders);
+    }
+
+    /**
+     * Sets file extension for template loader
+     *
+     * @param   string  $extension  Template files extension
+     * @return  $this
+     */
+// TODO : méthode à virer ???? et donc forcer dans le constructeur d'avoir un objet Engine déjà initialisé avec les bonnes extensions ????
+    public function setFileExtension(string $extension): self
+    {
+        $this->engine->setFileExtension($extension);
+        return $this;
+    }
+}
---
+++ tests/PlatesRendererTest.php
@@ -0,0 +1,124 @@
+<?php
+
+use League\Plates\Engine;
+use Chiron\Views\TemplatePath;
+use Chiron\Views\PlatesRenderer;
+use PHPUnit\Framework\TestCase;
+
+class PlatesRendererTest extends TestCase
+{
+    /**
+     * @var Engine
+     */
+    private $platesEngine;
+    /**
+     * @var bool
+     */
+    private $error;
+    public function setUp()
+    {
+        $this->error = false;
+        $this->platesEngine = new Engine();
+    }
+
+    public function assertTemplatePath($path, TemplatePath $templatePath, $message = null)
+    {
+        $message = $message ?: sprintf('Failed to assert TemplatePath contained path %s', $path);
+        $this->assertEquals($path, $templatePath->getPath(), $message);
+    }
+
+    public function assertTemplatePathString($path, TemplatePath $templatePath, $message = null)
+    {
+        $message = $message ?: sprintf('Failed to assert TemplatePath casts to string path %s', $path);
+        $this->assertEquals($path, (string) $templatePath, $message);
+    }
+
+    public function assertTemplatePathNamespace($namespace, TemplatePath $templatePath, $message = null)
+    {
+        $message = $message
+            ?: sprintf('Failed to assert TemplatePath namespace matched %s', var_export($namespace, true));
+        $this->assertEquals($namespace, $templatePath->getNamespace(), $message);
+    }
+
+    public function assertEmptyTemplatePathNamespace(TemplatePath $templatePath, $message = null)
+    {
+        $message = $message ?: 'Failed to assert TemplatePath namespace was empty';
+        $this->assertEmpty($templatePath->getNamespace(), $message);
+    }
+
+    public function assertEqualTemplatePath(TemplatePath $expected, TemplatePath $received, $message = null)
+    {
+        $message = $message ?: 'Failed to assert TemplatePaths are equal';
+        if ($expected->getPath() !== $received->getPath()
+            || $expected->getNamespace() !== $received->getNamespace()
+        ) {
+            $this->fail($message);
+        }
+    }
+
+    public function testCanProvideEngineAtInstantiation()
+    {
+        $renderer = new PlatesRenderer($this->platesEngine);
+        $this->assertInstanceOf(PlatesRenderer::class, $renderer);
+        $this->assertEmpty($renderer->getPaths());
+    }
+    public function testLazyLoadsEngineAtInstantiationIfNoneProvided()
+    {
+        $renderer = new PlatesRenderer();
+        $this->assertInstanceOf(PlatesRenderer::class, $renderer);
+        $this->assertEmpty($renderer->getPaths());
+    }
+    public function testCanAddPath()
+    {
+        $renderer = new PlatesRenderer();
+        $renderer->addPath(__DIR__ . '/TestAsset');
+        $paths = $renderer->getPaths();
+        $this->assertInternalType('array', $paths);
+        $this->assertCount(1, $paths);
+        $this->assertTemplatePath(__DIR__ . '/TestAsset', $paths[0]);
+        $this->assertTemplatePathString(__DIR__ . '/TestAsset', $paths[0]);
+        $this->assertEmptyTemplatePathNamespace($paths[0]);
+        return $renderer;
+    }
+    /**
+     * @param PlatesRenderer $renderer
+     * @depends testCanAddPath
+     */
+    public function testAddingSecondPathWithoutNamespaceIsANoopAndRaisesWarning($renderer)
+    {
+        $paths = $renderer->getPaths();
+        $path  = array_shift($paths);
+        set_error_handler(function ($error, $message) {
+            $this->error = true;
+            $this->assertContains('duplicate', $message);
+            return true;
+        }, E_USER_WARNING);
+        $renderer->addPath(__DIR__);
+        restore_error_handler();
+        $this->assertTrue($this->error, 'Error handler was not triggered when calling addPath() multiple times');
+        $paths = $renderer->getPaths();
+        $this->assertInternalType('array', $paths);
+        $this->assertCount(1, $paths);
+        $test = array_shift($paths);
+        $this->assertEqualTemplatePath($path, $test);
+    }
+    public function testCanAddPathWithNamespace()
+    {
+        $renderer = new PlatesRenderer();
+        $renderer->addPath(__DIR__ . '/TestAsset', 'test');
+        $paths = $renderer->getPaths();
+        $this->assertInternalType('array', $paths);
+        $this->assertCount(1, $paths);
+        $this->assertTemplatePath(__DIR__ . '/TestAsset', $paths[0]);
+        $this->assertTemplatePathString(__DIR__ . '/TestAsset', $paths[0]);
+        $this->assertTemplatePathNamespace('test', $paths[0]);
+    }
+
+    public function testDelegatesRenderingToUnderlyingImplementation()
+    {
+        $renderer = new PlatesRenderer();
+        $renderer->addPath(__DIR__ . '/TestAsset');
+        $result = $renderer->render('testTemplate', ['hello' => 'Hi']);
+        $this->assertEquals('Hi', $result);
+    }
+}
